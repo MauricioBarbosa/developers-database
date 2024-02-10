d769397c463f8f3e43710ab1fa38a263077de8e4
Mauricio Barbosa <mauriciobarboza6.1@gmail.com>
2024-02-10 17:29:04 -0300

    add: routes module

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+
+@NgModule({
+    imports: [],
+    declarations: [HeaderComponent, FooterComponent],
+    exports: [HeaderComponent, FooterComponent]
+})
+
+export class SharedModule {
+
+}
