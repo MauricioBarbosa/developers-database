c7ea3ca943e37febf6416815e21fe1cb6d1ee201
Mauricio Barbosa <mauriciobarboza6.1@gmail.com>
2024-02-26 19:22:38 -0300

    add: authServiceMock

--- frontend/src/app/shared/application/service/auth/auth.service.ts
+++ frontend/src/app/shared/application/service/auth/auth.service.ts
@@ -1,12 +1,14 @@
+import { Injectable } from '@angular/core';
 import { Result } from '@badrap/result';
 import { Observable } from 'rxjs';
 import { HttpService } from '../http/http.service';
 import { AuthenticationDto } from './dto/authentication.dto';
 
+Injectable();
 export class AuthService {
   private accessToken: string;
 
-  constructor(private httpService: HttpService) {}
+  constructor(private readonly httpService: HttpService) {}
 
   public authenticate(
     authenticationDto: AuthenticationDto
