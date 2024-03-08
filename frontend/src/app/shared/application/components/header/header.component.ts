8218615a471b8e397c2cd95ad5608cb85009b7a8
Mauricio Barbosa <mauriciobarboza6.1@gmail.com>
2024-03-08 10:55:01 -0300

    finished: menu

--- frontend/src/app/shared/application/components/header/header.component.ts
+++ frontend/src/app/shared/application/components/header/header.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../../service/auth/auth.service';
+import { Component } from "@angular/core";
+import { AuthService } from "../../service/auth/auth.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.css",
 })
 export class HeaderComponent {
   isAuthenticated: boolean;
+  toggledMenu: boolean = false;
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.isAuthenticated =
-      this.authService.isAuthenticated();
+    this.isAuthenticated = this.authService.isAuthenticated();
+  }
+
+  toggleMenu() {
+    this.toggledMenu = !this.toggledMenu;
   }
 }
