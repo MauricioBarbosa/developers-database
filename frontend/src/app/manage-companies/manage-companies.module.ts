812caebc758373da9ccf9ef4aa4ebaba24558456
Mauricio Barbosa <mauriciobarboza6.1@gmail.com>
2024-03-14 11:03:56 -0300

    finished: input search style

--- frontend/src/app/manage-companies/manage-companies.module.ts
+++ frontend/src/app/manage-companies/manage-companies.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from "@angular/core";
+import { NgxBootstrapIconsModule, plusCircle } from "ngx-bootstrap-icons";
 import { SharedModule } from "../shared/shared.module";
 import { SearchComponent } from "./components/search/search.component";
 import { ManageCompaniesRoutingModule } from "./manage-companies-routing.module";
 import { CompaniesListComponent } from "./pages/companies-list/companies-list.component";
 
 @NgModule({
-  imports: [SharedModule, ManageCompaniesRoutingModule],
+  imports: [
+    SharedModule,
+    ManageCompaniesRoutingModule,
+    NgxBootstrapIconsModule.pick({
+      plusCircle,
+    }),
+  ],
   declarations: [CompaniesListComponent, SearchComponent],
 })
 export class ManageCompaniesModule {}
